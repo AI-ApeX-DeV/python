2f451a556694eaac21f044d32c80b62a98257d5a
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-06 14:42:06 +0530

    done

--- list.py
+++ list.py
@@ -5,3 +5,37 @@ print(a[1:])
 print(a[:6])
 print(a[1:6:2])
 print(a[-1])
+
+a.clear()
+print(a)
+
+a.insert(0, 1)
+a.insert(1, 2)
+a.insert(2, 3)
+a.insert(3, 4)
+a.insert(4, 9)
+a.insert(5, 8)
+a.insert(6, 7)
+print(a)
+
+a.remove(2)
+
+print(a)
+
+a.pop(1)
+print(a)
+
+a.append(2)
+print(a)
+
+a.reverse()
+print(a)
+
+a.sort()
+print(a)
+
+a.sort(reverse=True)
+print(a)
+
+a.pop()
+print(a)
