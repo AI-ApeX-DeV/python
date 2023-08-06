4b40ee89464c43696d2899a275499ab9fbbfac72
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-06 16:48:08 +0530

    done

--- variable and address.py
+++ variable and address.py
@@ -6,3 +6,6 @@ print(id(b))
 
 c = a
 print(id(c))
+
+# if the number or a string is same then the id will be same
+# if data is not tagged to any variable then it will be deleted by garbage collector
