12a4f4b97136737b1610cb88bc7626e2fcd689f0
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-06 16:59:25 +0530

    done

---
+++ logical operators.py
@@ -0,0 +1,8 @@
+a = 5
+b = 9
+if (a < b and a == 5):
+    print("a is greater than b")
+if (a > b or a == 5):
+    print("a is greater than b")
+if (not (a > b or a == 5)):
+    print("a is greater than b")
---
+++ number system.py
@@ -0,0 +1,12 @@
+a = 5
+print(bin(a))
+print(0b10111)
+
+print(hex(a))
+print(0x10111)
+
+print(oct(a))
+print(0o10111)
+
+print(0b10111 + 0x10111 + 0o10111)
+print(0b10111 + 0x10111 + 0o10111 + 5)
