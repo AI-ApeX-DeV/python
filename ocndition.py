9bebf43413bb6eef82b1ee4fd7996d3464d2417b
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-06 22:51:41 +0530

    done

--- ocndition.py
+++ ocndition.py
@@ -5,3 +5,27 @@ elif a == 6:
     print("a is equal to 6")
 else:
     print("a is not equal to 5 or 6")
+
+
+for i in range(1, 10, 2):
+    print(i, end=" ")
+
+i = 0
+print()
+
+while i < 10:
+    print(i, end=" ")
+    i += 1
+
+print()
+
+for i in range(10):
+    if i == 5:
+        print("this is 5", end=" ")
+        continue
+        print("this is cool")
+    elif i == 8:
+        print("this is 8 now its time to end the loop", end=" ")
+        break
+    else:
+        print(i, end=" ")
