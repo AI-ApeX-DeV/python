2ec60eb88b4b8c01bc1636360e65431d633c76d8
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-06 14:56:38 +0530

    done

--- tuple.py
+++ tuple.py
@@ -4,3 +4,12 @@ print(tup)
 print(tup[1])
 
 print(tup.count(5))
+
+# elements cannot be added to a tuple but two tuples can be concatenated
+
+print(tup)
+
+tup1 = (1, 2, 3, 4, 5)
+
+tup3 = tup+tup1
+print(tup3)
