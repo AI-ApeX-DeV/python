d6fa4c5fa56f89671826b59a1c0956cb36e9d5b4
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-10 00:15:31 +0530

    done

--- array.py
+++ array.py
@@ -3,6 +3,16 @@ vals = array('i', [1, 2, 3, 4, 5, 6])
 print(vals)
 vals.reverse()
 print(vals)
-print(vals.buffer_info())
+print(vals.buffer_info())  # (address, size)
 print(vals.typecode)
 print(vals[0])
+
+
+a = array('i', [])
+n = int(input("Enter the length of the array: "))
+
+for i in range(n):
+    x = int(input("Enter the next value: "))
+    a.append(x)
+
+print(a)
