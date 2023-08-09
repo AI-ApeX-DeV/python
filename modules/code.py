87f32f54729e7d3d0ee300653e5724fa9b68cbe8
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-10 02:20:38 +0530

    done

---
+++ modules/code.py
@@ -0,0 +1,6 @@
+from my_module import *
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 25]
+print(even(a))
+print(odd(a))
+print(perfectsquare(a))
--- modules/my_module.py
+++ modules/my_module.py
@@ -1,10 +1,10 @@
 def even(a):
-    return list(filter(lambda x: any(x == p*p for p in range(2, x)), a))
+    return list(filter(lambda x: x % 2 == 0, a))
 
 
 def odd(a):
-    return list(filter(lambda x: all(x != p*p for p in range(2, x)), a))
+    return list(filter(lambda x: x % 2 != 0, a))
 
 
-def perfectsqaure(a):
+def perfectsquare(a):
     return list(filter(lambda x: any(x == p*p for p in range(2, x)), a))
