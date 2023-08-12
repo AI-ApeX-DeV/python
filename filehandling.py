b175dd69b1c19c623d15544514bfc3a00aca77ac
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-12 17:47:12 +0530

    done

--- filehandling.py
+++ filehandling.py
@@ -9,3 +9,28 @@ print(f.readline())
 print(f.readline())
 
 print(f.readline(4))
+f.close()
+
+print("lets explore something different\n")
+
+f = open("data.txt", "w")
+f.write("this is a new line\n this is the second line\n this is the third line\n this is the fourth line\n")
+f.close()
+
+print("lets explore something different\n")
+
+f = open("data.txt", "r")
+print(f.read())
+f.close()
+
+print("lets explore something different\n")
+
+f = open("data.txt", "a")
+f.write("this is the fifth line\n")
+f.close()
+
+print("lets explore something different\n")
+
+f = open("data.txt", "r")
+print(f.read())
+f.close()
